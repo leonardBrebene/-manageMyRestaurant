a591b2cb62f1775fa0a5a28070fe58fed548b8d6
leonardBrebene <brebene.leonard@yahoo.ro>
2021-05-17 22:52:42 +0300

    First commit

---
+++ demo/managemyrestaurant/src/main/java/com/restaurant/managemyrestaurant/ManagemyrestaurantApplication.java
@@ -0,0 +1,13 @@
+package com.restaurant.managemyrestaurant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManagemyrestaurantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManagemyrestaurantApplication.class, args);
+	}
+
+}
---
+++ demo/managemyrestaurant/src/test/java/com/restaurant/managemyrestaurant/ManagemyrestaurantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.restaurant.managemyrestaurant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManagemyrestaurantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
