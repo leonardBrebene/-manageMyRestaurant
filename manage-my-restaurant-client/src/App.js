3c0524c1f097b3370fc076c2c7aeef5c5cc8fbd7
leonardBrebene <brebene.leonard@yahoo.ro>
2021-06-08 22:45:58 +0300

    Added an element that could represent a table and could be dragged inside of a restaurant acording to its curent position

--- manage-my-restaurant-client/src/App.js
+++ manage-my-restaurant-client/src/App.js
@@ -1,24 +1,77 @@
-import logo from './logo.svg';
+import noShaddow from './images/320px-HD_transparent_picture.png'
+import cub from './images/cubul.png'
+
+import { useRef, useState } from 'react'
+import useDraggable from "./useDraggable";
 import './App.css';
 
-function App() {
+
+const DraggableCard = ({ children }) => {
+  const cardRef = useRef(null);
+  useDraggable(cardRef);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="card" ref={cardRef}>
+      {children}
     </div>
+  );
+};
+
+function App() {
+  const [ceva, setceva] = useState(false);
+  const [ceva2, setceva2] = useState(false);
+
+  function clearShaddow(e) {
+
+    let img = new Image();
+    img.src = noShaddow;
+    const drag = e.dataTransfer;
+    drag.setDragImage(img, 0, 0);
+
+    const cubul = document.getElementById('cub')
+    console.log(e.view.outerWidth)
+    console.log(e.clientX)
+    console.log(cubul.getBoundingClientRect())
+
+    console.log((e.clientX - (cubul.style.width / 2)) / e.view.outerWidth * 100)
+  }
+
+
+  function updatePosition(e) {
+    if (ceva === true && ceva2 === true && e.clientX % 2 === 0) {
+      const cubul = document.getElementById('cub')
+
+      console.log("positionchanged")
+      cubul.style.left = `${e.clientX - cubul.clientWidth / 2}px`
+      cubul.style.top = `${e.clientY - cubul.clientHeight / 2}px`
+    }
+  }
+
+
+
+  return (
+    <div className="App" style={{ backgroundColor: 'red' }}>
+      <p>Scrie ceva aici</p>
+      <DraggableCard>Card 1</DraggableCard>
+      <div
+        id='cub' draggable={true}
+        style={{ position: 'absolute', textAlign: 'center', border: '5px dotted black', justifyContent: 'center', display: '' }}
+
+        onDragStart={(e => { setceva(true); clearShaddow(e) })}
+        onDragOver={(e => { updatePosition(e) })}>
+        <img src={cub} alt='alternativa'
+          className='imaginea'
+          style={{ position: 'relative', textAlign: 'center', border: '5px dotted black' }}
+        ></img>
+      </div>
+      <button
+        style={{ position: 'absolute', right: '5px', bottom: '5px', zIndex: '50' }}
+        onClick={(() => { setceva2(!ceva2); console.log("setat") })}
+      >{JSON.stringify(ceva2)}</button>
+
+
+    </div>
+
   );
 }
 
